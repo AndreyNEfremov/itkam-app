673be576a3c4137d25bd028b5723216784c18965
“AndreyNEfremov” <postandry@gmail.com>
2020-02-22 22:43:09 +0300

    ls51_users_API

--- src/Redux/usersReducer.js
+++ src/Redux/usersReducer.js
@@ -4,9 +4,9 @@ const SET_USERS = 'SET-USERS';
 
 let initialState = {
     users: [
-        { id: 1, photoUrl: 'https://avavatar.ru/images/avatars/3/avatar_2ZuL6XVKffLhkSEy.jpg', followed: true, fullName: "Dmitry", status: 'bigBoss', location: { city: 'Minsk', country: 'Belarus' }},
-        { id: 2, photoUrl: 'https://avavatar.ru/images/avatars/3/avatar_tjxVEszfBZNdKe7F.jpg', followed: false, fullName: "Taras", status: 'Boss', location: { city: 'Kiev', country: 'Ukraine' }},
-        { id: 3, photoUrl: 'https://avavatar.ru/images/avatars/1/avatar_oC4jAgN51kPE3NGP.jpg', followed: false, fullName: "Valddemar", status: 'smallBoss', location: { city: 'Moscow', country: 'Russia' }}
+        // { id: 1, photoUrl: 'https://avavatar.ru/images/avatars/3/avatar_2ZuL6XVKffLhkSEy.jpg', followed: true, fullName: "Dmitry", status: 'bigBoss', location: { city: 'Minsk', country: 'Belarus' }},
+        // { id: 2, photoUrl: 'https://avavatar.ru/images/avatars/3/avatar_tjxVEszfBZNdKe7F.jpg', followed: false, fullName: "Taras", status: 'Boss', location: { city: 'Kiev', country: 'Ukraine' }},
+        // { id: 3, photoUrl: 'https://avavatar.ru/images/avatars/1/avatar_oC4jAgN51kPE3NGP.jpg', followed: false, fullName: "Valddemar", status: 'smallBoss', location: { city: 'Moscow', country: 'Russia' }}
     ]
 };
 
