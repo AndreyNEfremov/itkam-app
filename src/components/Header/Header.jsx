664a797696d219f8aca5a9acc5b38f08586c4c52
“AndreyNEfremov” <postandry@gmail.com>
2020-03-01 23:51:56 +0300

    ls61_login

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
 import classes from './Header.module.css';
+import { NavLink } from 'react-router-dom';
 
-const Header = () => {
+const Header = (props) => {
     return <header className={classes.header}>
         <a href="/"><img src='https://image.winudf.com/v2/image1/Y29tLnJhaW4uY29hc3RfaWNvbl8xNTYyMzIwMTI4XzAxMA/icon.png?w=100&fakeurl=1' alt='logo'></img></a>
+        <div className={classes.loginBlock}>
+            {props.isAuth ? props.login
+            : <NavLink to={'/login'}>Login</NavLink>}
+        </div>
     </header>
 };
 
